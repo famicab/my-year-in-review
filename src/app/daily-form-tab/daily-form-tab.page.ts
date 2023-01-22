3a0adf7757abcfe910115a7126af8c54c5264968
famicab <cesco.deamicis@gmail.com>
2023-01-22 16:02:19 +0100

    First commit

---
+++ src/app/daily-form-tab/daily-form-tab.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'daily-form-tab.page.html',
+  styleUrls: ['daily-form-tab.page.scss']
+})
+export class DailyFormTabPage {
+
+  constructor() {}
+
+}
